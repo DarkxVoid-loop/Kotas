9bb2cf05063978611ef62e7f87122ff29417f328
DarkxVoid-loop <83542837+DarkxVoid-loop@users.noreply.github.com>
2021-07-24 20:24:03 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import NavBar from "./components/Navbar";
+import Content from "./components/Content";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Content />
+    </div>
+  );
+}
+
+export default App;
